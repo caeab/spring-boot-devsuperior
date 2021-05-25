9811dc63fd3031127ecf2550a110884c905ca335
Caetano Borges <caetanoalcantara@gmail.com>
2021-05-24 22:44:40 -0300

    Category

---
+++ src/main/java/com/devsuperior/myfirstproject/resources/CategoryResource.java
@@ -0,0 +1,43 @@
+package com.devsuperior.myfirstproject.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.myfirstproject.entities.Category;
+import com.devsuperior.myfirstproject.repositories.CategoryRepository;
+
+@RestController // Indica que é uma aplicação Rest - para que o framework reconheça esse recurso rest
+@RequestMapping(value = "/categories") // para ter mapeamento URL
+public class CategoryResource {
+	
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@GetMapping // Diz que quando executar get nesse caminho vai ser chamado esse método
+	public ResponseEntity<List<Category>> findAll() {		
+		
+		List<Category> list = categoryRepository.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+		
+		Category cat = categoryRepository.findById(id);
+		
+		return ResponseEntity.ok().body(cat);
+	}
+	
+	
+	
+
+}
